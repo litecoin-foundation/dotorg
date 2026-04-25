4ad9bf27e7e4391a207e7c0fe93a0412f799ec72
losh11 <loshan1212@gmail.com>
2026-04-25 19:54:27 +0100

    0.21.5.4

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -35,39 +35,40 @@ const DownloadSection = ({ nodeType }: DownloadSectionProps) => {
 
   const litecoinCoreDownloads: DownloadInfo[] = [
     {
-      version: "27.1",
+      version: "0.21.5.4",
       platform: "Windows 64-bit",
-      size: "32.4 MB",
-      filename: "litecoin-27.1-win64-setup.exe",
+      size: "18.6 MB",
+      filename: "litecoin-0.21.5.4-win64-setup.exe",
       checksum:
-        "d89a5b5a1d5c8e4f2a3b8c7d6e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8",
-      url: "https://download.litecoin.org/litecoin-27.1/win/litecoin-27.1-win64-setup.exe",
+        "023b0ff0cdec7d440fb62d95aae4bf7b7c892242372d33de10e2f889c94c15ad",
+      url: "https://download.litecoin.org/litecoin-0.21.5.4/litecoin-0.21.5.4-win64-setup.exe",
     },
     {
-      version: "27.1",
+      version: "0.21.5.4",
       platform: "macOS",
-      size: "28.1 MB",
-      filename: "litecoin-27.1-osx64.dmg",
+      size: "14.2 MB",
+      filename: "litecoin-0.21.5.4-osx.dmg",
       checksum:
-        "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8d89a5b5a1d5c8e4f2a3b8c7d6e9f0",
-      url: "https://download.litecoin.org/litecoin-27.1/mac/litecoin-27.1-osx64.dmg",
+        "c6d21013c41d255e0dd2b2bd2f69395ef7d1ba3e1cfcad41cbf39a4f97078526",
+      url: "https://download.litecoin.org/litecoin-0.21.5.4/litecoin-0.21.5.4-osx.dmg",
     },
     {
-      version: "27.1",
+      version: "0.21.5.4",
       platform: "Linux 64-bit",
-      size: "31.7 MB",
-      filename: "litecoin-27.1-x86_64-linux-gnu.tar.gz",
+      size: "35.9 MB",
+      filename: "litecoin-0.21.5.4-x86_64-linux-gnu.tar.gz",
       checksum:
-        "b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8d89a5b5a1d5c8e4f2a3b8c7d6e9f0a1",
-      url: "https://download.litecoin.org/litecoin-27.1/linux/litecoin-27.1-x86_64-linux-gnu.tar.gz",
+        "91621306bafcadeebc266c264c95576536b5b2658e0ba03b05262ed4b9ab611f",
+      url: "https://download.litecoin.org/litecoin-0.21.5.4/litecoin-0.21.5.4-x86_64-linux-gnu.tar.gz",
     },
     {
-      version: "27.1",
+      version: "0.21.5.4",
       platform: "Linux ARM64",
-      size: "29.3 MB",
-      filename: "litecoin-27.1-aarch64-linux-gnu.tar.gz",
-      checksum: "c3d4e5f6a7b8c9d0e1f2a3b8c7d6e9f0a1b2",
-      url: "https://download.litecoin.org/litecoin-27.1/linux/litecoin-27.1-aarch64-linux-gnu.tar.gz",
+      size: "34.6 MB",
+      filename: "litecoin-0.21.5.4-aarch64-linux-gnu.tar.gz",
+      checksum:
+        "f0213853817d0ba7854aa718dc43bf991aba80a7db8b47969ae979dc083acce2",
+      url: "https://download.litecoin.org/litecoin-0.21.5.4/litecoin-0.21.5.4-aarch64-linux-gnu.tar.gz",
     },
   ];
 
@@ -101,7 +102,8 @@ const DownloadSection = ({ nodeType }: DownloadSectionProps) => {
     },
   ];
 
-  const allDownloads = nodeType === "full-node" ? litecoinCoreDownloads : electrumLTCDownloads;
+  const allDownloads =
+    nodeType === "full-node" ? litecoinCoreDownloads : electrumLTCDownloads;
 
   // Detect user's OS
   const detectOS = (): string => {
@@ -147,9 +149,10 @@ const DownloadSection = ({ nodeType }: DownloadSectionProps) => {
     });
   };
 
-  const downloadButtonText = nodeType === "full-node"
-    ? `Download Litecoin Core ${primaryDownload.version}`
-    : `Download Electrum-LTC ${primaryDownload.version}`;
+  const downloadButtonText =
+    nodeType === "full-node"
+      ? `Download Litecoin Core ${primaryDownload.version}`
+      : `Download Electrum-LTC ${primaryDownload.version}`;
 
   return (
     <div className="space-y-6">
